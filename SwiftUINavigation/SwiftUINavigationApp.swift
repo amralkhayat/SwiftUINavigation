a2afbf0a7c99e598f4a8eabccb3991075beb571a
Amr Al-khayat <33759960+amralkhayat@users.noreply.github.com>
2025-07-14 18:38:02 +0200

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Amr Saeed on 14.07.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
